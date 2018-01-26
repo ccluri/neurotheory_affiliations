99c95e1cb5d1145d81c71f01be2bcfeb2e6caee3
Chaitanya Chintaluri <ccluri@gmail.com>
2018-01-26 11:25:15 +0000

    reformating

--- affiliation_utils.py
+++ affiliation_utils.py
@@ -3,128 +3,99 @@ import time
 import pickle
 import os
 import csv
-from difflib import SequenceMatcher
+# from difflib import SequenceMatcher
 from geopy.geocoders import Nominatim
-# import json
-
-# data = json.load(open('grid.json'))
-# entries = data['institutes']
-# num_inst = len(entries)
 
 
 def consolidated_names():
-    all_ids, all_insti, all_insti_city, all_insti_country = populate_names()
-    alias_ids, alias_names = populated_alias()
-    labels_ids, labels_names = populate_labels()
-    return all_ids, all_insti, all_insti_city, all_insti_country, alias_ids, alias_names, labels_ids, labels_names
+    i_g_dict, g_i_dict, g_city_dict, g_country_dict = populate_names()
+    alias_g_dict = populated_alias()
+    labels_g_dict = populate_labels()
+    output = (i_g_dict, g_i_dict, g_city_dict, g_country_dict,
+              alias_g_dict, labels_g_dict)
+    return output
 
 
 def populate_names():
-    all_ids = []
-    all_insti = []
-    all_insti_city = []
-    all_insti_country = []
+    i_g_dict = {}
+    g_i_dict = {}
+    g_city_dict = {}
+    g_country_dict = {}
     with open('grid.csv', 'r') as h:
         reader = csv.reader(h, delimiter=',')
         for row in reader:
-            all_ids.append(row[0])
-            all_insti.append(row[1].lower())
-            all_insti_city.append(row[2].lower())
-            all_insti_country.append(row[4].lower())
-    return all_ids, all_insti, all_insti_city, all_insti_country
+            i_g_dict[row[1].lower()] = row[0]
+            g_i_dict[row[0]] = row[1].lower()
+            g_city_dict[row[0]] = row[2].lower()
+            g_country_dict[row[0]] = row[4].lower()
+    return i_g_dict, g_i_dict, g_city_dict, g_country_dict
 
 
 def populated_alias():
-    alias_ids = []
-    alias_names = []
+    alias_g_dict = {}
     with open(os.path.join('.', 'full_tables', 'aliases.csv')) as h:
         reader = csv.reader(h, delimiter=',')
         for row in reader:
-            alias_ids.append(row[0].lower())
-            alias_names.append(row[1].lower())
-    return alias_ids, alias_names
+            alias_g_dict[row[1].lower()] = row[0]
+    return alias_g_dict
 
 
 def populate_labels():
-    labels_ids = []
-    labels_names = []
+    labels_g_dict = {}
     with open(os.path.join('.', 'full_tables', 'labels.csv')) as h:
         reader = csv.reader(h, delimiter=',')
         for row in reader:
-            labels_ids.append(row[0].lower())
-            labels_names.append(row[1].lower())
-    return labels_ids, labels_names
-
-
-def closest_match(name_list, test_all):
-    # test_all = all_insti + alias_names
-    candidates = []
-    for rname in name_list:
-        name = rname.lower()
-        s = sorted(test_all, key=lambda x: SequenceMatcher(None, x, name).ratio(), reverse=True)
-        candidates.append(s[0])
-        print(rname, s[0:2])
-    return candidates
+            labels_g_dict[row[1].lower()] = row[0]
+    return labels_g_dict
 
 
 def fetch_text_loc(name_list):
     gids = []
-    not_found = []
-    candidates = []
-    all_ids, all_insti, all_insti_city, all_insti_country, alias_ids, alias_names, labels_ids, labels_names = consolidated_names()
+    found_idx = []
     not_found_idx = []
+    i_g, g_i, g_city, g_country, a_g, l_g = consolidated_names()
     for idw, rname in enumerate(name_list):
         name = rname.lower()
-        try:
-            name_idx = all_insti.index(name)
-            gids.append(all_ids[name_idx])
-        except ValueError:  # check in aliases
-            try:
-                alias_idx = alias_names.index(name)
-                gids.append(alias_ids[alias_idx])
-            except ValueError:
-                try:
-                    labels_idx = labels_names.index(name)
-                    gids.append(labels_ids[labels_idx])
-                except ValueError:
-                    gids.append('NULL')
-                    not_found_idx.append(idw)
-                    not_found.append(name)
-
+        if name in i_g:
+            gids.append(i_g[name])
+            found_idx.append(idw)
+        elif name in a_g:
+            gids.append(a_g[name])
+            found_idx.append(idw)
+        elif name in l_g:
+            gids.append(l_g[name])
+            found_idx.append(idw)
+        else:
+            gids.append('NULL')
+            not_found_idx.append(idw)
     insti_list = []
     city_list = []
     country_list = []
-    for idx, gid in enumerate(gids):
+    for gid in gids:
         if gid != 'NULL':
-            insti_list.append(all_insti[idx])
-            city_list.append(all_insti_city[idx])
-            country_list.append(all_insti_country[idx])
+            insti_list.append(g_i[gid])
+            city_list.append(g_city[gid])
+            country_list.append(g_country[gid])
         else:
             insti_list.append('NULL')
             city_list.append('NULL')
             country_list.append('NULL')
-    print(len(not_found), len(name_list))
-    # if not_found:
-    #     print('Not found for : ', not_found)
-    #     print('Now searching for candidates')
-    #     candidates = closest_match(not_found)
-    #     print('Did you mean : ', candidates)
-    return insti_list, city_list, country_list, not_found, not_found_idx, candidates
+    print(len(not_found_idx), len(name_list))
+    return gids, insti_list, city_list, country_list
 
 
 def fetch_loc(city_list, country_list):
     assert(len(city_list) == len(country_list))
     geolocator = Nominatim()
     locs = []
+    print('Fetching geo locations - may take some time')
     for ii, name in enumerate(city_list):
-        if ii % 20 == 0 :
-            time.sleep(3)
+        time.sleep(0.2)
         if name != 'NULL':
             locs.append(geolocator.geocode(name + ', ' +
                                            country_list[ii]))
-            print locs[-1].longitude
         else:
-            locs.append(False)
+            locs.append('NULL')
     return locs
 
 
@@ -142,10 +113,8 @@ def fetch_coauth_names(auth_list, ln_nos, email_list, co_auth):
     return coauth_names, email_names
 
 
-def fetch_cosyne_affliations():
-    # all_abstracts = ['2012.txt', '2013.txt', '2014.txt',
-    #                  '2015.txt', '2016.txt', '2017.txt']
-    all_abstracts = ['2013.txt']
+def fetch_cosyne_affliations(this_year):
+    all_abstracts = [this_year]
     big_list = {}
     for this_year in all_abstracts:
         with open(os.path.join('..', 'cosyne_analysis', 'cosyne',
@@ -160,34 +129,67 @@ def fetch_cosyne_affliations():
                                                        email_list, co_auth)
         # year = int(this_year.rstrip('.txt'))
     # print len(auth_list), auth_list, len(auth_aff)
-    return auth_aff, aff_idx, coauth_names, title_list, title_ids
+    return auth_aff, aff_idx, coauth_names, title_list, title_ids, email_names
 
 
-auth_aff, aff_idx, coauth_names, title_list, title_ids = fetch_cosyne_affliations()  # list of list
-un_resolved_list = []
-for sub_list in auth_aff:
-    for aff in sub_list:   # ' '.join(mystring.split())
-        new = aff.strip()
-        un_resolved_list.append(' '.join(new.split()))
-        # print un_resolved_list[-1]
-# print un_resolved_list
-insti_list, city_list, country_list, not_found, not_found_idx, candidates = fetch_text_loc(un_resolved_list)
-print insti_list, city_list
-# for ii, noidea in enumerate(not_found):
-#     print noidea, candidates[ii]
-locs = fetch_loc(city_list, country_list)
-lat_list = []
-long_list = []
+def give_me_cosyne(this_year):
+    auth_aff, aff_idx, coauth_names, title_list, title_ids, email_names = fetch_cosyne_affliations(this_year)  # list of list
+    unres_aff_list = []
+    title_aff_count_dict = {}
+    aff_count = 0
+    for title_id, title_aff_list in enumerate(auth_aff):
+        title_aff_count_dict[title_id] = []
+        for aff in title_aff_list:
+            new = ' '.join(aff.strip().split())
+            unres_aff_list.append(new)
+            title_aff_count_dict[title_id].append(aff_count)
+            aff_count += 1
+    gids, insti_list, city_list, country_list = fetch_text_loc(unres_aff_list)
+    locs = fetch_loc(city_list, country_list)
+    lat_list = []
+    long_list = []
+    for loc in locs:
+        if loc != 'NULL':
+            try:
+                lat_list.append(loc.latitude)
+                long_list.append(loc.longitude)
+            except AttributeError:
+                lat_list.append('NULL')
+                long_list.append('NULL')
+        else:
+            lat_list.append('NULL')
+            long_list.append('NULL')
+    # print len(insti_list), len(auth_aff)
+    auth_count = 0
+    with open(os.path.join('..', 'cosyne_analysis', 'cosyne',
+                           this_year.strip('.txt') + '_aff.csv'), 'wb') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',')
+        csvwriter.writerow(['ID', 'Email'] +
+                           ['Author', 'Affiliation'] +
+                           ['Aff resolved', 'GRID'] +
+                           ['City', 'Country', 'Latitude', 'Longitude'])
+        for idx in range(len(title_list)):
+            for kk, author in enumerate(coauth_names[idx]):
+                this_email = email_names[idx][kk]
+                affiliations = [int(pp)-1 for pp in aff_idx[auth_count]]
+                t_aff_count = 0
+                for affliation in affiliations:
+                    sp_idx = title_aff_count_dict[idx][t_aff_count]
+                    csvwriter.writerow([title_ids[idx]] + [this_email] + [author] +
+                                       [auth_aff[idx][affliation], insti_list[sp_idx]] +
+                                       [gids[sp_idx], city_list[sp_idx], country_list[sp_idx]] +
+                                       [lat_list[sp_idx], long_list[sp_idx]])
+                    t_aff_count += 1
+                auth_count += 1
+    print('Finished :', this_year)
 
-for loc in locs:
-    if not loc:
-        lat_list.append(loc.latitude)
-        long_list.append(loc.longitude)
-    else:
-        lat_list.append('NULL')
-        long_list.append('NULL')
-print lat_list, long_list
 
+# all_abstracts = ['2012.txt', '2013.txt', '2014.txt',
+#                  '2015.txt', '2016.txt', '2017.txt']
+all_abstracts = ['2014.txt',
+                 '2015.txt', '2016.txt', '2017.txt']
+for abstract in all_abstracts:
+    give_me_cosyne(abstract)
 
 # i, city, country = fetch_text_loc(['Oxford university',
 #                                    'University of oxford',
