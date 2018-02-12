4d159fe52edb3897f16966401a6aa48078b44284
chaitu@soledad <ccluri@gmail.com>
2018-02-12 08:59:53 +0000

    2018 updates

--- fix_names.py
+++ fix_names.py
@@ -157,6 +157,104 @@ def resolve_names_in_file(filename='authorsUnresolvedAff.csv', exceptions_filena
                     writer.writerow([noidea, uinput.lower()])
                     # print("completed percent : ", (100. * float(idx)) / float(len(new_aff_list)))
 
+                    
+def victor_exceptions_2018(filename='affiliationsNotInDB_2018.csv', exceptions_filename='exceptions_alias.csv'):
+    already_parsed = {}
+    if os.path.isfile(exceptions_filename):
+        with open('exceptions_alias.csv', 'rt') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=':')
+            for row in spamreader:
+                old, new = row
+                already_parsed[old] = new
+    i_g_dict, g_i_dict, g_city_dict, g_country_dict, alias_g_dict, labels_g_dict = utils.consolidated_names()
+    all_insti = list(i_g_dict.keys())
+    alias_names = list(alias_g_dict.keys())
+    labels_names = list(labels_g_dict.keys())
+    all_names = all_insti + alias_names + labels_names
+    completer = MyCompleter(all_names)
+    readline.set_completer_delims('')
+    readline.set_completer(completer.complete)
+    readline.parse_and_bind('tab: complete')
+
+    start_pt = 0
+    end_pt = 65
+
+    with open(filename, 'rt') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        aff_list = []
+        for ii, row in enumerate(reader):
+            if ii != 0:
+                aff_list.append(row[0].lower())
+    with open('exceptions_alias.csv', 'at') as csvfile:
+        writer = csv.writer(csvfile, delimiter=':')
+        new_aff_list = aff_list[start_pt:end_pt]
+        for id_prev, noidea in enumerate(new_aff_list):
+            idx = id_prev + start_pt
+            if noidea not in already_parsed.keys():
+                testname = simple_mistakes(noidea)
+                if testname in all_names:
+                    print('We know this name')
+                    if testname in all_insti:
+                        uinput = testname
+                    else:
+                        try:
+                            uinput = g_i_dict[alias_g_dict[testname]]
+                        except KeyError:
+                            uinput = g_i_dict[labels_g_dict[testname]]
+                else:
+                    bing.search(noidea, 3)
+                    print(idx)
+                    uinput = input(noidea + ' | Correction :  ', )
+                if uinput != '':
+                    already_parsed[noidea] = uinput.lower()
+                    writer.writerow([noidea, uinput.lower()])
+                    # print("completed percent : ", (100. * float(idx)) / float(len(new_aff_list)))
+
+
+def victor_2018(filename='affiliationsNotInDB_2018.csv', new_filename='resolvedAff2018.csv'):
+    name_exceptions = utils.exceptions_alias()
+    with open(filename, 'rt') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        aff_list = []
+        for ii, row in enumerate(reader):
+            if ii != 0:
+                aff_list.append(row[0].lower())
+    print(aff_list)
+    i_g, g_i, g_city, g_country, a_g, l_g = utils.consolidated_names()
+    gid_list = []
+    city_list = []
+    country_list = []
+    for raff in aff_list:
+        aff = raff.lower()
+        if aff in name_exceptions.keys():
+            new_gid = utils.resolve_name(name_exceptions[aff], i_g, a_g, l_g)
+            if new_gid != 'NULL':
+                gid_list.append(new_gid)
+                city_list.append(g_city[new_gid])
+                country_list.append(g_country[new_gid])
+            else:
+                gid_list.append('')
+                city_list.append('')
+                country_list.append('')                
+        else:
+            gid_list.append('')
+            city_list.append('')
+            country_list.append('')
+    gid_latlong_dict = utils.update_gid_dict(gid_list, city_list, country_list)
+    with open(new_filename, 'wt') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerow(['affiliation', 'Aff resolved', 'GRID'] +
+                        ['City', 'Country', 'Latitude', 'Longitude'])
+        for idx, gid in enumerate(gid_list):
+            if gid != '':
+                writer.writerow([aff_list[idx], g_i[gid], gid, city_list[idx], country_list[idx]] +
+                                [str(jj) for jj in gid_latlong_dict[gid]])
+            else:
+                writer.writerow([aff_list[idx], '', '', '', ''] +
+                                ['', ''])
+
+    return
+
 # print simple_mistakes('universityof manchester')
 # print simple_mistakes('oxford university')
 
@@ -194,4 +292,5 @@ def resolve_names_in_file(filename='authorsUnresolvedAff.csv', exceptions_filena
 # print "You entered", input
 
 
-
+victor_exceptions_2018()
+victor_2018()
